18465254467e38121b8a0e7f08f7f0f59c3510ea
ChrisDiannee <cdcalamba02.cdc@gmail.com>
2017-09-02 18:46:50 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+//server.js
+//require modules
+var express = require('express');
+var path = require('path');
+//instantiate express
+var app = express();
+//set port
+app.set('port', (process.env.PORT || 5000));
+//use static files
+app.use(express.static(path.join(__dirname, 'public/')));
+//express routes
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'view/a.html'));
+});
+app.get('/a.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'view/a.html'));
+});
+app.get('/mid2.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'view/mid2.html'));
+});
+app.get('/midterm.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'view/midterm.html'));
+});
+app.get('/modal-example.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'view/modal-example.html'));
+});
+
+
+
+
+
+//express server listen
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port ',app.get('port'));
+});
